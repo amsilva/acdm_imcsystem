a32654c0fae4ff59962bad565c7876be8ba04013
amsilva <andre.marcos@gmail.com>
2020-05-13 06:24:32 +0000

    entidade Avaliacao

--- Avaliacao.java
+++ Avaliacao.java
@@ -1,3 +1,24 @@
 class Avaliacao {
 
+  private float peso;
+  private float altura;
+
+  public Avaliacao(float p, float a) {
+    this.peso = p;
+    this.altura = a;
+  }
+
+  public float indice() {
+    retunr this.peso / (this.altura * this.altura);
+  }
+
+  public String clasificacao(){
+    if (this.indice() < 18) 
+      return "MAGRO";
+    else if (this.indice() > 30) 
+      return "OBESO";
+    else 
+      return "REGULAR";
+  }
+
 }
